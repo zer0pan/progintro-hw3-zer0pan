3a12a5dfc5d9cd6e02130d8c88b713ccb988c4c9
cpanaret <xristospan1577@gmail.com>
2026-01-09 01:06:23 +0200

    2nd commit

--- goengine/src/types.h
+++ goengine/src/types.h
@@ -1,5 +1,12 @@
 #define MAX_BOARD 19
 
+typedef enum
+{
+    EMPTY = 0,
+    BLACK = 1,
+    WHITE = 2
+} Stone;
+
 typedef struct
 {
     int size;
@@ -8,10 +15,3 @@ typedef struct
     int cons_passes; // 0,1 or 2
     int game_over;   // 0 or 1
 } GameState;
-
-typedef enum
-{
-    EMPTY = 0,
-    BLACK = 1,
-    WHITE = 2
-} Stone;
